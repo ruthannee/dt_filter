44d5d988c5fde4c0d5fb6cc60f7ee0560231deb9
Ruth Anne <anne.carvalho@yahoo.com.br>
2021-10-14 15:21:45 -0300

    formatting file to export

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import re
+from xlsxwriter.utility import xl_rowcol_to_cell
 
 #Importando o dataset
 dataset = pd.read_excel('20210922 GSBDs_Produtivo_Fechamento (Produccion).xlsx', sheet_name="Part Numbers")
@@ -45,9 +46,35 @@ fornecedor['FINAL'] = dsATENDE[~dsATENDE['Part number'].isin(fornecedor['Part nu
 print(fornecedor.columns)
 
 dsATENDE = pd.merge(dsATENDE, fornecedor, how='inner', left_on='Part number', right_on='FINAL')
-dsATENDE.rename(columns={'Part number_x' : 'Part number', 'Part number_y': 'PD Original'}, inplace=True)
+dsATENDE.rename(columns={'Part number_x' : 'Part number', 'Part number_y': 'PN Fornecedor'}, inplace=True)
 del dsATENDE['FINAL']
 
 #EXPORTAÇÃO FINAL
-dsATENDE.to_excel('Novo Fornecedores AT.xlsx', sheet_name="Fornecedores", header=True, index=False)
+writer = pd.ExcelWriter('Novo Fornecedores AT.xlsx', engine='xlsxwriter')
+dsATENDE.to_excel(writer, sheet_name="Fornecedores", header=True, index=False)
 dsNAO_ATENDE.to_excel('Novo Fornecedores NA.xlsx', sheet_name="Fornecedores", header=True, index=False)
+
+workbook = writer.book
+worksheet = writer.sheets['Fornecedores']
+worksheet.set_zoom(100)
+worksheet.set_column('A:D', 15)
+worksheet.set_column('H:N', 15)
+number_rows = len(dsATENDE.index)
+color_range = "N2:N{}".format(number_rows+1)
+worksheet.set_column('E:G', None, None, {'hidden': True})
+worksheet.set_column('J:K', None, None, {'hidden': True})
+
+obsolete = workbook.add_format({'bg_color': '#FFC7CE',
+                               'font_color': '#9C0006'})
+active = workbook.add_format({'bg_color': '#C6EFCE',
+                               'font_color': '#006100'})
+worksheet.conditional_format(color_range, {'type':     'cell',
+                                    'criteria': 'equal to',
+                                    'value':    '"OBSOLETO"',
+                                    'format':   obsolete})
+worksheet.conditional_format(color_range, {'type':     'cell',
+                                    'criteria': 'equal to',
+                                    'value':    '"ATIVO"',
+                                    'format':   active})
+
+writer.save()
